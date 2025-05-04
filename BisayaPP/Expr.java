19ede57bdb969663aaf409512017dfaff928087f
CodexPremiera <ashleyken.professional@gmail.com>
2025-05-04 15:12:55 +0800

    annotate GenerateAST and organize code in Expr and Stmt

--- BisayaPP/Expr.java
+++ BisayaPP/Expr.java
@@ -6,142 +6,152 @@ import java.util.List;
 
 abstract class Expr {
     interface Visitor<R> {
-    R visitAssignExpr(Assign expr);
-    R visitBinaryExpr(Binary expr);
-    R visitGroupingExpr(Grouping expr);
-    R visitLiteralExpr(Literal expr);
-    R visitLogicalExpr(Logical expr);
-    R visitUnaryExpr(Unary expr);
-    R visitVariableExpr(Variable expr);
-    R visitAssignBisExpr(AssignBis expr);
-    R visitPostfixExpr(Postfix expr);
-    }
-  static class Assign extends Expr {
-    Assign(Token name, Expr value) {
-      this.name = name;
-      this.value = value;
+        R visitAssignExpr(Assign expr);
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitLogicalExpr(Logical expr);
+        R visitUnaryExpr(Unary expr);
+        R visitVariableExpr(Variable expr);
+        R visitAssignBisExpr(AssignBis expr);
+        R visitPostfixExpr(Postfix expr);
     }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitAssignExpr(this);
+    static class Assign extends Expr {
+        final Token name;
+        final Expr value;
+
+        Assign(Token name, Expr value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitAssignExpr(this);
+        }
     }
 
-    final Token name;
-    final Expr value;
-  }
-  static class Binary extends Expr {
-    Binary(Expr left, Token operator, Expr right) {
-      this.left = left;
-      this.operator = operator;
-      this.right = right;
+    static class Binary extends Expr {
+        final Expr left;
+        final Token operator;
+        final Expr right;
+
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
     }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitBinaryExpr(this);
+    static class Grouping extends Expr {
+        final Expr expression;
+
+        Grouping(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
     }
 
-    final Expr left;
-    final Token operator;
-    final Expr right;
-  }
-  static class Grouping extends Expr {
-    Grouping(Expr expression) {
-      this.expression = expression;
+    static class Literal extends Expr {
+        final Object value;
+
+        Literal(Object value) {
+            this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
+        }
     }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitGroupingExpr(this);
+    static class Logical extends Expr {
+        final Expr left;
+        final Token operator;
+        final Expr right;
+
+        Logical(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLogicalExpr(this);
+        }
     }
 
-    final Expr expression;
-  }
-  static class Literal extends Expr {
-    Literal(Object value) {
-      this.value = value;
+    static class Unary extends Expr {
+        final Token operator;
+        final Expr right;
+
+        Unary(Token operator, Expr right) {
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
     }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitLiteralExpr(this);
+    static class Variable extends Expr {
+        final Token name;
+
+        Variable(Token name) {
+            this.name = name;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitVariableExpr(this);
+        }
     }
 
-    final Object value;
-  }
-  static class Logical extends Expr {
-    Logical(Expr left, Token operator, Expr right) {
-      this.left = left;
-      this.operator = operator;
-      this.right = right;
+    static class AssignBis extends Expr {
+        final Token type;
+        final Token name;
+        final Expr value;
+
+        AssignBis(Token type, Token name, Expr value) {
+            this.type = type;
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitAssignBisExpr(this);
+        }
     }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitLogicalExpr(this);
+    static class Postfix extends Expr {
+        final Expr left;
+        final Token operator;
+
+        Postfix(Expr left, Token operator) {
+            this.left = left;
+            this.operator = operator;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitPostfixExpr(this);
+        }
     }
 
-    final Expr left;
-    final Token operator;
-    final Expr right;
-  }
-  static class Unary extends Expr {
-    Unary(Token operator, Expr right) {
-      this.operator = operator;
-      this.right = right;
-    }
-
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitUnaryExpr(this);
-    }
-
-    final Token operator;
-    final Expr right;
-  }
-  static class Variable extends Expr {
-    Variable(Token name) {
-      this.name = name;
-    }
-
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitVariableExpr(this);
-    }
-
-    final Token name;
-  }
-  static class AssignBis extends Expr {
-    AssignBis(Token type, Token name, Expr value) {
-      this.type = type;
-      this.name = name;
-      this.value = value;
-    }
-
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitAssignBisExpr(this);
-    }
-
-    final Token type;
-    final Token name;
-    final Expr value;
-  }
-  static class Postfix extends Expr {
-    Postfix(Expr left, Token operator) {
-      this.left = left;
-      this.operator = operator;
-    }
-
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitPostfixExpr(this);
-    }
-
-    final Expr left;
-    final Token operator;
-  }
 
     abstract <R> R accept(Visitor<R> visitor);
 }
--- Tool/GenerateAST.java
+++ Tool/GenerateAST.java
@@ -5,113 +5,141 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
 
-// [12]
+/**
+ * [12] This tool auto-generates the abstract syntax tree (AST) classes used
+ * in the Bisaya++ interpreter/compiler. It outputs Java files like Expr.java and Stmt.java
+ * with nested classes representing various language constructs.
+ */
 public class GenerateAST {
-    public static void main(String[] args) throws IOException{
-        if(args.length != 1){
+
+    public static void main(String[] args) throws IOException {
+        // Ensure the user supplies exactly one command-line argument (output directory).
+        if (args.length != 1) {
             System.err.println("Usage: generate_ast <output directory>");
             System.exit(64);
         }
+
         String outputDir = args[0];
 
-        defineAST(outputDir,"Expr",Arrays.asList(
-            "Assign  : Token name, Expr value",
-            "Binary   : Expr left, Token operator, Expr right",
-            "Grouping : Expr expression",
-            "Literal  : Object value",
-            "Logical  : Expr left, Token operator, Expr right",
-            "Unary    : Token operator, Expr right",
-            "Variable : Token name" 
-            // BISAYA++
-            , "AssignBis: Token type, Token name, Expr value"
-            ,"Postfix   : Expr left, Token operator"
+        // Generate the Expr.java AST class with all expression types.
+        defineAST(outputDir, "Expr", Arrays.asList(
+                "Assign     : Token name, Expr value",
+                "Binary     : Expr left, Token operator, Expr right",
+                "Grouping   : Expr expression",
+                "Literal    : Object value",
+                "Logical    : Expr left, Token operator, Expr right",
+                "Unary      : Token operator, Expr right",
+                "Variable   : Token name",
+
+                // BISAYA++ extensions
+                "AssignBis  : Token type, Token name, Expr value",
+                "Postfix    : Expr left, Token operator"
         ));
 
-        defineAST(outputDir,"Stmt",Arrays.asList(
-            "Block      : List<Stmt> statements",
-            "Expression : Expr expression"
-            ,"Print      : Expr expression"
-            ,"Var        : Token name, Expr initializer"
-            ,"If         : Expr condition, Stmt thenBranch," +
-            " Stmt elseBranch",
-            "While       : Expr condition, Stmt body"
+        // Generate the Stmt.java AST class with all statement types.
+        defineAST(outputDir, "Stmt", Arrays.asList(
+                "Block      : List<Stmt> statements",
+                "Expression : Expr expression",
+                "Print      : Expr expression",
+                "Var        : Token name, Expr initializer",
+                "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
+                "While      : Expr condition, Stmt body",
 
-            // BISAYA++
-            ,"Ipakita    : Expr expression"
-            ,"Mugna      : Token type, List<Token> names, List<Expr> initializers"
-            ,"Kung       : Expr condition, Stmt thenBranch, Stmt elseBranch"
-            ,"Sugod       : List<Stmt> statements"
-            ,"Dawat       : List<Token> names"
-            ,"Mintras       : Expr condition, Stmt body"
-            
+                // BISAYA++ extensions
+                "Ipakita    : Expr expression",
+                "Mugna      : Token type, List<Token> names, List<Expr> initializers",
+                "Kung       : Expr condition, Stmt thenBranch, Stmt elseBranch",
+                "Sugod      : List<Stmt> statements",
+                "Dawat      : List<Token> names",
+                "Mintras    : Expr condition, Stmt body"
         ));
     }
-    // [15]
-    private static void defineAST(String outputDir, String baseName, List<String> types) throws IOException{
-        String path = outputDir + "/"+baseName+".java";
-        PrintWriter writer = new PrintWriter(path,"UTF-8");
 
-        writer.println("package BisayaPP;");
-        writer.println();
-        writer.println("import java.util.List;");
-        writer.println();
-        writer.println("// [16]");
-        writer.println();
+    /**
+     * [15] Generates the source code for a single AST base class (e.g., Expr or Stmt),
+     * along with all its concrete subclasses.
+     *
+     * @param outputDir Directory to save the generated Java file.
+     * @param baseName  Name of the base abstract class (Expr or Stmt).
+     * @param types     List of subclass declarations in the form "ClassName : fieldType fieldName, ..."
+     */
+    private static void defineAST(String outputDir, String baseName, List<String> types) throws IOException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+        // Write package and imports
+        writer.println("package BisayaPP;\n");
+        writer.println("import java.util.List;\n");
+        writer.println("// [16]\n");
+
+        // Declare abstract base class
         writer.println("abstract class " + baseName + " {");
 
+        // Generate the Visitor interface inside the base class
         defineVisitor(writer, baseName, types);
 
+        // Generate each subclass as a static nested class
         for (String type : types) {
             String className = type.split(":")[0].trim();
-            String fields = type.split(":")[1].trim(); 
+            String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
 
-        writer.println();
-        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+        // Declare the abstract accept() method for visitors
+        writer.println("\n    abstract <R> R accept(Visitor<R> visitor);");
 
+        // End base class
         writer.println("}");
         writer.close();
-        
     }
-    // [14]
-    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types){
+
+    /**
+     * [14] Defines the Visitor<R> interface inside the AST base class.
+     * Each visit method corresponds to a concrete subclass of the AST.
+     */
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
         writer.println("    interface Visitor<R> {");
-        for(String type : types){
+        for (String type : types) {
             String typeName = type.split(":")[0].trim();
-            writer.println("    R visit"+typeName+baseName+"("+ typeName+ " "+ baseName.toLowerCase()+");");
+            writer.println("        R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
         }
-        writer.println("    }");
+        writer.println("    }\n");
     }
-    // [13]
-    private static void defineType(
-        PrintWriter writer, String baseName,
-        String className, String fieldList) {
-        writer.println("  static class " + className + " extends " +  baseName + " {");
-  
-      // Constructor.
-        writer.println("    " + className + "(" + fieldList + ") {");
-  
-      // Store parameters in fields.
+
+    /**
+     * [13] Defines a concrete AST subclass inside the base class.
+     * Example: class Binary extends Expr { ... }
+     *
+     * @param writer     Writer to output the code.
+     * @param baseName   The name of the base class (Expr or Stmt).
+     * @param className  The name of the subclass.
+     * @param fieldList  Comma-separated list of fields (e.g., "Expr left, Token operator").
+     */
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("    static class " + className + " extends " + baseName + " {");
+
+        // Define fields
         String[] fields = fieldList.split(", ");
         for (String field : fields) {
-            String name = field.split(" ")[1];
-            writer.println("      this." + name + " = " + name + ";");
+            writer.println("        final " + field + ";");
         }
-  
-        writer.println("    }");
+        writer.println();
 
-        writer.println();
-        writer.println("    @Override");
-        writer.println("    <R> R accept(Visitor<R> visitor) {");
-        writer.println("    return visitor.visit"+className+baseName+"(this);");
-        writer.println("    }");
-  
-        // Fields.
-        writer.println();
+        // Define constructor
+        writer.println("        " + className + "(" + fieldList + ") {");
         for (String field : fields) {
-            writer.println("    final " + field + ";");
+            String name = field.split(" ")[1]; // Extract variable name
+            writer.println("            this." + name + " = " + name + ";");
         }
-        writer.println("  }");
+        writer.println("        }\n");
+
+        // Implement accept() method for the visitor
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
+
+        // End class
+        writer.println("    }\n");
     }
 }
